30dce314edc310157f7efcade9ce36507e8ef714
KirillPikov <pikov_kirya@bk.ru>
2019-12-30 18:11:25 +0400

    Update PathSettings.java

--- src/main/java/com/mediasoft/bookstore/config/PathSettings.java
+++ src/main/java/com/mediasoft/bookstore/config/PathSettings.java
@@ -6,7 +6,7 @@ package com.mediasoft.bookstore.config;
 public final class PathSettings {
     //-----------------------------< Request Params >--------------------------------------------//
     /**
-     * Название параметра, отвечающего за номер страницы при пагинации.
+     * Название параметра, отвечающего за ID страницы при пагинации.
      */
     public final static String PAGE_REQUEST_PARAM = "page";
 
@@ -30,54 +30,80 @@ public final class PathSettings {
      * Название переменной ID покупателя в адресной строке.
      */
     public final static String CUSTOMER_ID_PATH_VAR_NAME = "customerId";
+
     /**
      * Название переменной ID автора в адресной строке.
      */
     public final static String AUTHOR_ID_PATH_VAR_NAME = "authorId";
+
+    /**
+     * Название переменной ID книги в адресной строке.
+     */
+    public final static String BOOK_ID_PATH_VAR_NAME = "bookId";
+
     /**
      * Название переменной ID издателя в адресной строке.
      */
     public final static String PUBLISHER_ID_PATH_VAR_NAME = "publisherId";
+
     /**
      * Название переменной ID склада в адресной строке.
      */
-    public final static String WAREHOUSE_ID_PATH_VAR_NAME = "publisherId";
+    public final static String WAREHOUSE_ID_PATH_VAR_NAME = "warehouseId";
+
     /**
-     * Название переменной страницы корзины в адресной строке.
+     * Название переменной ID склада в адресной строке.
      */
-    public final static String SHOPPING_BASKET_NUM_PATH_VAR_NAME = "shoppingBasketNum";
+    public final static String WAREHOUSE_BOOK_ID_PATH_VAR_NAME = "warehouseBookId";
+
     /**
-     * Название переменной страницы книги в корзине в адресной строке.
+     * Название переменной ID корзины в адресной строке.
      */
-    public final static String SHOPPING_BASKET_BOOK_NUM_PATH_VAR_NAME = "shoppingBasketBookNum";
+    public final static String SHOPPING_BASKET_ID_PATH_VAR_NAME = "shoppingBasketId";
+
+    /**
+     * Название переменной ID книги в корзине в адресной строке.
+     */
+    public final static String SHOPPING_BASKET_BOOK_ID_PATH_VAR_NAME = "shoppingBasketBookId";
 
     //-----------------------------< Controller paths >------------------------------------------//
     /**
      * Адрес для контроллера Customer. {@link com.mediasoft.bookstore.controller.CustomerController}
      */
     public final static String CUSTOMER_CONTROLLER_PATH = "customers";
+
     /**
      * Адрес для контроллера ShoppingBasket. {@link com.mediasoft.bookstore.controller.ShoppingBasketController}
      */
     public final static String SHOPPING_BASKET_CONTROLLER_PATH = CUSTOMER_CONTROLLER_PATH + "/{" + CUSTOMER_ID_PATH_VAR_NAME + "}/" + "shoppingBaskets";
+
     /**
      * Адрес для контроллера ShoppingBasketBook. {@link com.mediasoft.bookstore.controller.ShoppingBasketBookController}
      */
-    public final static String SHOPPING_BASKET_BOOK_CONTROLLER_PATH = SHOPPING_BASKET_CONTROLLER_PATH + "/{" + SHOPPING_BASKET_NUM_PATH_VAR_NAME + "}/" + "shoppingBasketBooks";
+    public final static String SHOPPING_BASKET_BOOK_CONTROLLER_PATH = SHOPPING_BASKET_CONTROLLER_PATH + "/{" + SHOPPING_BASKET_ID_PATH_VAR_NAME + "}/" + "shoppingBasketBooks";
+
     /**
      * Адрес для контроллера Customer. {@link com.mediasoft.bookstore.controller.CustomerController}
      */
     public final static String AUTHOR_CONTROLLER_PATH = "authors";
+
     /**
      * Адрес для контроллера Publisher. {@link com.mediasoft.bookstore.controller.PublisherController}
      */
     public final static String PUBLISHER_CONTROLLER_PATH = "publishers";
+
     /**
      * Адрес для контроллера Warehouse. {@link com.mediasoft.bookstore.controller.WarehouseController}
      */
     public final static String WAREHOUSE_CONTROLLER_PATH = "warehouses";
+
     /**
-     * Адрес для контроллера Book. {@link com.mediasoft.bookstore.controller} //TODO BookController
+     * Адрес для контроллера WarehouseBook. {@link com.mediasoft.bookstore.controller.WarehouseBookController}
+     */
+    public final static String WAREHOUSE_BOOK_CONTROLLER_PATH = WAREHOUSE_CONTROLLER_PATH + "/{" + WAREHOUSE_ID_PATH_VAR_NAME + "}/" + "warehouseBooks";
+
+    /**
+     * Адрес для контроллера Book. {@link com.mediasoft.bookstore.controller.BookController}
      */
     public final static String BOOK_CONTROLLER_PATH = "books";
 

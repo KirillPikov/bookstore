96d707d0fe5f59189a693dec823fb2ff06a84ff3
KirillPikov <pikov_kirya@bk.ru>
2019-12-30 18:11:37 +0400

    Create ShoppingBasketReleaseException.java

---
+++ src/main/java/com/mediasoft/bookstore/exception/ShoppingBasketReleaseException.java
@@ -0,0 +1,8 @@
+package com.mediasoft.bookstore.exception;
+
+public class ShoppingBasketReleaseException extends RuntimeException {
+
+    public ShoppingBasketReleaseException(String message) {
+        super(message);
+    }
+}
